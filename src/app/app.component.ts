4d95265808623e9d8f9863a491a77c91ccb09abc
8i-ght <needboosts3545@gmail.com>
2024-01-12 12:44:11 -0600

    fix errors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  // @ViewChild('f') NgForm;
+  defaultQuestion = 'pet';
+
   suggestUserName() {
     const suggestedName = 'Superuser';
   }
 
-  onSubmit(form: HTMLFormElement) {
-    
+  onSubmit() {
+    // console.log(this.signupForm);
   }
 }
